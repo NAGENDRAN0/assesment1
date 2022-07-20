b7c086211da24afca18abe6eaf01b97fca3d2ede
NAGENDRAN0 <109665849+NAGENDRAN0@users.noreply.github.com>
2022-07-20 23:26:44 +0530

    Add files via upload

---
+++ Mobile.java
@@ -0,0 +1,26 @@
+package week1.day2;
+
+public class Mobile {
+	public void sendMessage() {
+		// TODO Auto-generated method stub
+		System.out.println("sendMessage");
+	}
+	public void ShareDocument() {
+		// TODO Auto-generated method stub
+		System.out.println("shareDocument");
+	}
+     public void call() {
+		// TODO Auto-generated method stub
+    	 System.out.println("call");
+
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Mobile mob=new Mobile();
+		mob.sendMessage();
+		mob.ShareDocument();
+		mob.call();
+
+	}
+
+}
---
+++ Mycar.java
@@ -0,0 +1,30 @@
+package week1.day2;
+public class Mycar {
+	 public void Break() { 
+	    	System.out.println("applybreak");
+	    }
+	    public void gear() {
+	    	System.out.println("applygear");
+	    }
+	    public void Ac() {
+			// TODO Auto-generated method stub
+         System.out.println("switchOnAc");
+		}
+	    public void accelerate() {
+			// TODO Auto-generated method stub
+         System.out.println("aapplyAccelerate");
+		}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Mycar c1=new Mycar();
+		c1.Break();
+		c1.gear();
+		c1.accelerate();
+		c1.Ac();
+				
+				
+		
+
+	}
+
+}
